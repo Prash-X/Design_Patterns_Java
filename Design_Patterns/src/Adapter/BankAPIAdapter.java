84ad9c15ddaa10fee966677a30f7287193983177
PrashantG <gidnavarp@gmail.com>
2024-02-08 00:22:45 +0530

    Adapter_DP

---
+++ Design_Patterns/src/Adapter/BankAPIAdapter.java
@@ -0,0 +1,8 @@
+package Adapter;
+
+public interface BankAPIAdapter {
+    public String transfer(String toUserName, String fromUserName, double amount);
+    public double checkBalance(String userName);
+    public boolean authenticate(String userName);
+
+}
---
+++ Design_Patterns/src/Adapter/YesBankAdapter.java
@@ -0,0 +1,29 @@
+package Adapter;
+
+import Adapter.thirdparty.YesBankAPI;
+
+public class YesBankAdapter implements BankAPIAdapter{
+
+    private YesBankAPI yesBankAPI;
+    public YesBankAdapter(){
+        this.yesBankAPI = new YesBankAPI();
+    }
+    @Override
+    public String transfer(String toUserName, String fromUserName, double amount) {
+        char result = yesBankAPI.transfer(toUserName,fromUserName,(int)amount);
+        if(result=='D')
+            return "Success";
+        else
+            return "Failure";
+    }
+
+    @Override
+    public double checkBalance(String userName) {
+        return yesBankAPI.checkBalance(userName);
+    }
+
+    @Override
+    public boolean authenticate(String userName) {
+        return yesBankAPI.authenticate(userName)=='Y';
+    }
+}
